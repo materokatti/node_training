f874ef2822433d0531291635cc9531a95c521434
Doyun <materokatti@gmail.com>
2024-09-26 08:20:19 +0900

    add : lib/collectAnswers.js

---
+++ ask.js
@@ -0,0 +1,11 @@
+const collectAnswers = require("./lib/collectAnswers")
+
+const questions = [
+    "What is your name?",
+    "Where do you live?",
+    "What are you going to do with Node.js?"
+];
+
+collectAnswers(questions, answers => {
+    console.log(answers);
+})
